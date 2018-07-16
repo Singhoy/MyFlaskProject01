3fcc84e54440f23cbc842f01603f07c92354915e
Singhoi <380309472@qq.com>
2018-07-16 23:53:08 +0800

    '创建蓝图'

--- manage.py
+++ manage.py
@@ -5,15 +5,12 @@ from info import db, create_app
 # 创建app，并传入配置模式：dev/pro
 app = create_app('dev')
 
+# 添加扩展命令行
 manager = Manager(app)
+
+# 数据库迁移
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-
-
-@app.route('/')
-def index():
-    return 'index'
-
 
 if __name__ == '__main__':
     manager.run()
