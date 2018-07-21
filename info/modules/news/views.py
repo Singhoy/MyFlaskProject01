aef465cb3640ac8a509d4377357770ff08ade963
Singhoi <380309472@qq.com>
2018-07-21 15:49:57 +0800

    '详情页新闻数据展示功能完善'

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, session, current_app, g
+from flask import render_template, session, current_app, g, abort
 
-from info.models import User
+from info import constants
+from info.models import News
 from info.modules.news import news_blu
 from info.utils.common import func_out
 
@@ -8,18 +9,32 @@ from info.utils.common import func_out
 @news_blu.route('/<int:news_id>')
 @func_out
 def news_detail(news_id):
-    # # 获取到当前登录用户的id
-    # user_id = session.get("user_id")
-    #
-    # # 通过id获取用户信息
-    # user = None
-    # if user_id:
-    #     try:
-    #         user = User.query.get(user_id)
-    #     except Exception as e:
-    #         current_app.logger.error(e)
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        abort(404)
+
+    if not news:
+        # 返回数据未找到的页面
+        abort(404)
+
+    news.clicks += 1
+
+    # 获取点击排行数据
+    news_list = None
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.looger.error(e)
+
+    click_news_list = []
+    for news in news_list if news_list else []:
+        click_news_list.append(news.to_basic_dict())
 
     data = {
+        "news": news.to_dict(),
+        "click_news_list": click_news_list,
         "user_info": g.user.to_dict() if g.user else None
     }
 
