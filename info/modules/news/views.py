71ff9eb29c17779c437fa6b2255f360aa251882c
Singhoi <380309472@qq.com>
2018-07-22 19:35:59 +0800

    '完善新闻收藏功能'

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,9 +1,55 @@
-from flask import render_template, session, current_app, g, abort
+from flask import render_template, session, current_app, g, abort, request, jsonify
 
-from info import constants
+from info import constants, db
 from info.models import News
 from info.modules.news import news_blu
 from info.utils.common import func_out
+
+from info.utils.response_code import RET
+
+
+# 收藏视图
+@news_blu.route("/news_collect", methods=['POST'])
+@func_out
+def news_collect():
+    """新闻收藏"""
+
+    user = g.user
+    json_data = request.json
+    news_id = json_data.get("news_id")
+    action = json_data.get("action")
+
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    if not news_id:
+        return jsonify(errno=RET.PARAMERR, errmsg="新闻id参数错误")
+
+    if action not in ("collect", "cancel_collect"):
+        return jsonify(errno=RET.PARAMERR, errmsg="action参数错误")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="新闻数据不存在")
+
+    if action == "collect":
+        user.collection_news.append(news)
+    else:
+        user.collection_news.remove(news)
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存失败")
+
+    return jsonify(errno=RET.OK, errmsg="操作成功")
 
 
 @news_blu.route('/<int:news_id>')
@@ -32,9 +78,17 @@ def news_detail(news_id):
     for news in news_list if news_list else []:
         click_news_list.append(news.to_basic_dict())
 
+    # 判断是否收藏该新闻，默认为 false
+    is_collected = False
+    # 判断用户是否收藏过该新闻
+    if g.user:
+        if news in g.user.collection_news:
+            is_collected = True
+
     data = {
         "news": news.to_dict(),
         "click_news_list": click_news_list,
+        "is_collected": is_collected,
         "user_info": g.user.to_dict() if g.user else None
     }
 
