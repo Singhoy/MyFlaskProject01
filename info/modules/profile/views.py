526eba9886cd53212dee03ce81209c33da2e59b5
Singhoi <380309472@qq.com>
2018-07-24 00:13:30 +0800

    '完成用户新闻收藏功能'

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,11 +1,51 @@
 from flask import g, redirect, render_template, request, jsonify, current_app, session
 
 from info import db
-from info.constants import QINIU_DOMIN_PREFIX
+from info.constants import QINIU_DOMIN_PREFIX, USER_COLLECTION_MAX_NEWS
 from info.utils.common import func_out
 from info.utils.image_storage import storage
 from info.utils.response_code import RET
 from . import profile_blu
+
+
+# 新闻收藏
+@profile_blu.route('/collection')
+@func_out
+def user_collection():
+    # 获取页数
+    p = request.args.get("p", 1)
+    try:
+        p = int(p)
+    except Exception as e:
+        current_app.logger.error(e)
+        p = 1
+
+    user = g.user
+    collections = []
+    current_page = 1
+    total_page = 1
+    try:
+        # 进行分页数据查询
+        paginate = user.collection_news.paginate(p, USER_COLLECTION_MAX_NEWS, False)
+        # 获取分页数据
+        collections = paginate.items
+        # 获取当前页
+        current_page = paginate.page
+        # 获取总页数
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 收藏列表
+    collection_dict_li = []
+    for news in collections:
+        collection_dict_li.append(news.to_basic_dict())
+
+    data = {"total_page": total_page,
+            "current_page": current_page,
+            "collections": collection_dict_li}
+
+    return render_template('news/user_collection.html', data=data)
 
 
 # 修改用户密码
