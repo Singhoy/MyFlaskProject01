1fbf764163367fd3554702ba2b81f0b9dd6a9c0c
Singhoi <380309472@qq.com>
2018-07-23 21:11:30 +0800

    '完善个人中心用户基本资料'

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,9 +1,58 @@
-from flask import g, redirect, render_template
+from flask import g, redirect, render_template, request, jsonify, current_app, session
 
+from info import db
 from info.utils.common import func_out
+from info.utils.response_code import RET
 from . import profile_blu
 
 
+# 用户基本信息路由
+@profile_blu.route('/base_info', methods=["GET", "POST"])
+@func_out
+def base_info():
+    """
+    用户基本信息
+    1. 获取用户登录信息
+    2. 获取到传入参数
+    3. 更新并保存数据
+    4. 返回结果
+    """
+
+    # 1. 获取当前登录用户的信息
+    user = g.user
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', data={"user_info": user.to_dict()})
+
+    # 2.获取到传入参数
+    data_dict = request.json
+    nick_name = data_dict.get("nick_name")
+    gender = data_dict.get("gender")
+    signature = data_dict.get("signature")
+    if not all([nick_name, gender, signature]):
+        return jsonify(errno=RET.PARAMERR, errmsg="all参数有误")
+
+    if gender not in (['MAN', 'WOMAN']):
+        return jsonify(errno=RET.PARAMERR, errmsg="gender参数有误")
+
+    # 3.更新并保存数据
+    user.nick_name = nick_name
+    user.gender = gender
+    user.signature = signature
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存数据失败")
+
+    # 将session中保存的数据进行实时更新
+    session["nick_name"] = nick_name
+
+    # 4.返回响应
+    return jsonify(errno=RET.OK, errmsg="更新成功")
+
+
+# 个人中心主页
 @profile_blu.route('/info')
 @func_out
 def user_info():
--- info/static/news/js/user_base_info.js
+++ info/static/news/js/user_base_info.js
@@ -6,20 +6,45 @@ function getCookie(name) {
 $(function () {
 
     $(".base_info").submit(function (e) {
-        e.preventDefault()
+        e.preventDefault();
 
-        var signature = $("#signature").val()
-        var nick_name = $("#nick_name").val()
-        var gender = $(".gender").val()
+        var signature = $("#signature").val();
+        var nick_name = $("#nick_name").val();
+        var gender = $(".gender").val();
 
         if (!nick_name) {
-            alert('请输入昵称')
+            alert('请输入昵称');
             return
         }
         if (!gender) {
             alert('请选择性别')
         }
 
-        // TODO 修改用户信息接口
+        // 修改用户信息接口
+        const params = {
+            signature,
+            nick_name,
+            gender
+        };
+
+        $.ajax({
+            url:"/user/base_info",
+            type:"post",
+            contentType:"application/json",
+            headers:{
+                "X-CSRFToken":getCookie("csrf_token")
+            },
+            data:JSON.stringify(params)
+        })
+            .done(res =>{
+                if (res.errno == 0) {
+                    // 更新父窗口内容
+                    $('.user_center_name', parent.document).html(params['nick_name']);
+                    $('#nick_name', parent.document).html(params['nick_name']);
+                    $('.input_sub').blur()
+                }else {
+                    alert(res.errmsg)
+                }
+            })
     })
-})
+});
