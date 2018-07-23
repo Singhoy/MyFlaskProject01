21fd8e681cf6db476657c1238dca881fd2b549ba
Singhoi <380309472@qq.com>
2018-07-23 22:58:54 +0800

    '完善用户头像上传功能'

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,9 +1,48 @@
 from flask import g, redirect, render_template, request, jsonify, current_app, session
 
 from info import db
+from info.constants import QINIU_DOMIN_PREFIX
 from info.utils.common import func_out
+from info.utils.image_storage import storage
 from info.utils.response_code import RET
 from . import profile_blu
+
+
+# 头像上传路由
+@profile_blu.route('/pic_info', methods=["GET", "POST"])
+@func_out
+def pic_info():
+    user = g.user
+    if request.method == "GET":
+        return render_template('news/user_pic_info.html', data={"user_info": user.to_dict()})
+
+    # 1.获取到上传的文件
+    try:
+        avatar_file = request.files.get("avatar").read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="读取文件出错")
+
+    # 2.再将文件上传到七牛云
+    try:
+        url = storage(avatar_file)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="上传图片错误")
+
+    # 3.将头像信息更新到当前用户的模型中
+    # 设置用户模型相关数据
+    user.avatar_url = url
+    # 将数据保存到数据库
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存用户数据错误")
+
+    # 4.返回上传的结果（avatar_url）
+    return jsonify(errno=RET.OK, errmsg="上传成功", data={"avatar_url": QINIU_DOMIN_PREFIX + url})
 
 
 # 用户基本信息路由
--- info/static/news/js/user_pic_info.js
+++ info/static/news/js/user_pic_info.js
@@ -6,8 +6,24 @@ function getCookie(name) {
 
 $(function () {
     $(".pic_info").submit(function (e) {
-        e.preventDefault()
+        e.preventDefault();
 
-        //TODO 上传头像
+        // 上传头像
+        $(this).ajaxSubmit({
+            url: "/user/pic_info",
+            type: "POST",
+            headers: {
+                "X-CSRFToken": getCookie('csrf_token')
+            },
+            success: res => {
+                if (res.errno == 0) {
+                    $(".now_user_pic").attr("src", res.data.avatar_url);
+                    $(".user_center_pic>img", parent.document).attr("src", res.data.avatar_url);
+                    $(".user_login>img", parent.document).attr("src", res.data.avatar_url)
+                } else {
+                    alert(res.srrmsg)
+                }
+            }
+        })
     })
-})
+});
